809646089a7e4ed96593cc2d47f4fb6c85d73997
ErIrsyadK25 <52070105+ErIrsyadK25@users.noreply.github.com>
2019-07-27 22:31:30 +0700

    Update 4.php

--- 4.php
+++ 4.php
@@ -3,4 +3,6 @@ function countVowels($string){
 		echo $count= substr_count($string, 'a')+substr_count($string, 'i')+substr_count($string, 'u')+substr_count($string, 'e')+substr_count($string, 'o');
 	}
 countVowels("programmer");
+echo "<br />";
+countVowels("hmm..");
 ?>
